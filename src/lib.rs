306d03ca886f38561e51977887cb75ba4c8f5c3f
zhangguokai <zhangguokai@inspur.com>
2024-08-25 21:56:36 +0800

    fix:0.0.2

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,10 @@ mod req;
 mod vars;
 mod middle;
 mod ctx;
+mod util;
 
 pub use req::*;
 pub use vars::*;
 pub use middle::*;
 pub use ctx::*;
+pub use util::*;
---
+++ src/util.rs
@@ -0,0 +1,2 @@
+use bmbp_http_type::PageData;
+use bmbp_rdbc_orm::RdbcPage;
